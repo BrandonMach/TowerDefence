13bc658630a1528e9e2665b933eaa9c7818d15ee
Brandon Mach <brandon.mach2003@gmail.com>
2021-12-06 11:37:18 +0100

    Add project files.

---
+++ TowerDefence/Game1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Spline;
+using System.Diagnostics;
+
+namespace TowerDefence
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public SimplePath simplePath;
+        float moveUpSpline;
+        float t;
+
+        Vector2 monkeyPos;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            SpriteManager.LoadSprites(Content);
+            simplePath = new SimplePath(GraphicsDevice);
+            _graphics.PreferredBackBufferWidth = 1900;
+            _graphics.PreferredBackBufferHeight = 1000;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            moveUpSpline += 5;
+
+            monkeyPos = simplePath.GetPos(simplePath.beginT + moveUpSpline); // start of spline + ökar t med moveUpSline
+            Debug.WriteLine(simplePath.endT);
+            if (moveUpSpline >= simplePath.endT)
+            {
+                Debug.WriteLine("end");
+
+            }
+            simplePath.AddPoint(new Vector2(30, 10));
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            simplePath.Draw(_spriteBatch);
+            simplePath.DrawPoints(_spriteBatch);
+            if (!(moveUpSpline >= simplePath.endT))
+            {
+                Debug.WriteLine("end");
+                _spriteBatch.Draw(SpriteManager.bloonsMonkey, monkeyPos, null, Color.White, 0f, new Vector2(SpriteManager.bloonsMonkey.Width / 2, SpriteManager.bloonsMonkey.Height / 2), 1f, SpriteEffects.None, 1f);
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TowerDefence/SpriteManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TowerDefence
+{
+    class SpriteManager
+    {
+        
+            public static Texture2D bloonsMonkey;
+
+
+            public static void LoadSprites(ContentManager Content)
+            {
+                bloonsMonkey = Content.Load<Texture2D>("bloonsMonkey");
+            }
+        
+    }
+
+
+}
